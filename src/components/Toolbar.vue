59c3d9197cbbdacdd00d1d48a3f35ea3e5efea1a
Marshall Thompson <marshall@creativeideal.net>
2021-02-25 14:41:23 -0700

    Example login and logout buttons

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex flex-row items-center justify-center p-2 space-x-5">
+    <button v-if="$auth0.state.isLoading" class="button" type="button">
+      Loading
+    </button>
+    <div v-else>
+      <button
+        v-if="$auth0.state.isAuthenticated"
+        class="button"
+        type="button"
+        @click="logout"
+      >
+        Logout
+      </button>
+      <button v-else class="button" type="button" @click="login">Login</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, inject } from "vue";
+
+const $auth0: any = inject("$auth0");
+
+defineProps({
+  msg: String,
+});
+
+function login() {
+  // https://auth0.github.io/auth0-spa-js/classes/auth0client.html#loginwithredirect
+  $auth0.client.loginWithRedirect();
+}
+function logout() {
+  // https://auth0.github.io/auth0-spa-js/interfaces/logoutoptions.html
+  $auth0.client.logout({ returnTo: window.location.origin });
+}
+</script>
+
+<style lang="postcss" scoped>
+.button {
+  @apply bg-blue-600 text-white px-4 py-2 rounded-lg;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,12 +2,12 @@
   <main
     class="px-4 py-4 text-center text-gray-700 bg-gray-100 dark:text-gray-200"
   >
+    <Toolbar />
+
     <div class="m-auto mb-5 text-sm text-center opacity-25">
       [Default Layout]
     </div>
 
     <router-view />
-
-    <!-- <Footer /> -->
   </main>
 </template>
