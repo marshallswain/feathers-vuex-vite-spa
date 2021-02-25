921024e269aa0175a6f37659ce3fccd6a2462c80
Marshall Thompson <marshall@creativeideal.net>
2021-02-25 14:12:08 -0700

    Show auth state in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="text-center text-gray-300">[App.vue]</div>
   <router-view></router-view>
+  <div class="p-5 m-6">
+    <h2 class="text-xl font-bold">Auth State</h2>
+    <pre class="text-xs text-white bg-black">{{ authState }}</pre>
+  </div>
 </template>
 
 <script lang="ts">
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 // import { inject } from "vue";
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 export default {
   name: "App",
   setup() {
-    return { hi: "hi" };
+    const { state } = useStore();
+    const authState = computed(() => state.auth);
+    return { authState };
   },
 };
 </script>
